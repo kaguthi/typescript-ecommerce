229bd12bc3f22dc936baf6cdb4365a898943d07e
kaguthi <peterkaguathi873@gmail.com>
2024-07-06 18:41:17 +0300

    added protected route

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './page/Home'
 import Signin from './page/auth/Signin'
 import Layout from './page/Layout'
@@ -11,26 +11,29 @@ import Product from './page/Product/Product'
 import AddProduct from './page/Product/AddProduct'
 import EditProduct from './page/Product/EditProduct'
 import DeleteProduct from './page/Product/DeleteProduct'
+import { useAuth } from './page/auth/AuthContext'
 
 function Router() {
+
+  const { token } = useAuth();
 
   return (
     <>
       <Routes>
         <Route element={ <Layout /> }>
-          <Route index element={ <Home /> } />
-          <Route path='signin' element={ <Signin /> } />
-          <Route path='signup' element={ <SignUp />}/>
+          <Route index element={ token ? <Home /> :<Navigate to="signin" /> } />
+          <Route path='signin' element={ !token ? <Signin /> : < Navigate to="/" /> }/>
+          <Route path='signup' element={ !token ? <SignUp /> : < Navigate to="/" /> }/>
           <Route path='product'>
-            <Route index  element={ <Product /> } />
-            <Route path='addProduct' element={ <AddProduct />} />
-            <Route path='editProduct' element={ <EditProduct />} />
-            <Route path='deleteProduct' element={ <DeleteProduct />} />
+            <Route index  element={ <Product />} />
+            <Route path='addProduct' element={ token ? <AddProduct /> : <Navigate to="signin" /> } />
+            <Route path='editProduct' element={ token ? <EditProduct /> : <Navigate to="signin" /> } />
+            <Route path='deleteProduct' element={ token ? <DeleteProduct /> : <Navigate to="signin" />} />
           </Route>
           <Route path='user'>
-            <Route index element={ <User />} />
-            <Route path='edit' element={ <Edit />} />
-            <Route path='delete' element={ <Delete />} />
+            <Route index element={ <User /> } />
+            <Route path='edit' element={ token ? <Edit /> : <Navigate to="signin" />} />
+            <Route path='delete' element={ token ? <Delete /> : <Navigate to="signin" />} />
           </Route>
           <Route path='*' element={ < NotFound/> }/>
         </Route>
