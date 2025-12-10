ca8d4dfc9c764bc8e834d667b12e3a09f9b9327b
kaguthi <peterkaguathi873@gmail.com>
2025-12-10 15:48:30 +0300

    feat(server): add welcome message endpoint to greet users

--- server/server.js
+++ server/server.js
@@ -75,6 +75,9 @@ app.use(cors({
 }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
+app.use((req, res, next) => {
+  res.send("Welcome to E-buy")
+})
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
